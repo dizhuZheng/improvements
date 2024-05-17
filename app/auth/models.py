fbfa19dfdb584071faabe8058fefb434c8325ec7
dizhuZheng <dizhu210@gmail.com>
2024-05-17 13:54:17 -0400

    small mistake on user_loader"

--- app/auth/models.py
+++ app/auth/models.py
@@ -34,8 +34,7 @@ class User(UserMixin, db.Model):
     def __repr__(self): 
         return f'Role(\'{self.name}\', {self.roles})'
 
-      
-    
+       
 @login_manager.user_loader
 def load_user(id):
     user = User.query.get(int(id))
